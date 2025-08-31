eca8746c78ab954fbeff887a792b731aa22bd478
BrunoMazeto4 <brunosantosmazeto@gmail.com>
2025-08-31 16:00:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let heroName = "Batman"
+let heroXp = 6500
+let heroLevel
+
+if (heroXp <= 1000) {
+  heroLevel = "Ferro"
+} else if (heroXp >= 1001 && heroXp <= 2000) {
+  heroLevel = "Bronze"
+} else if (heroXp >= 2001 && heroXp <= 5000) {
+  heroLevel = "Prata"
+} else if (heroXp >= 5001 && heroXp <= 7000) {
+  heroLevel = "Ouro"
+} else if (heroXp >= 7001 && heroXp <= 8000) {
+  heroLevel = "Platina"
+} else if (heroXp >= 8001 && heroXp <= 9000) {
+  heroLevel = "Ascendente"
+} else if (heroXp >= 9001 && heroXp <= 10000) {
+  heroLevel = "Imortal"
+} else {
+  heroLevel = "Radiante"
+}
+
+console.log(
+  `O Herói de nome ${heroName}, que possuí ${heroXp} de experiência (XP), está no nível ${heroLevel}`
+)
